4ebee2f856968c03ec3cb9291dc9e4cd2ebdc589
Krishnaidnani <krishnaidnani7@gmail.com>
2024-12-12 16:18:15 +0530

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors"); // Import cors
 const faceCountRouter = require("./face_count");
 const speechBrightnessRouter = require("./class_quality");
 
 const app = express();
 const PORT = 3000;
+
+// Enable CORS
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", faceCountRouter);
